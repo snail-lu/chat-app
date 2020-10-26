0b3a9a495cbecf896faafb6e13ad92a334b2b912
路明 <snaillu@yeah.net>
2020-10-26 15:11:35 +0800

    注册页面调整

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import { WingBlank, List, InputItem, WhiteSpace, Radio, Button, Checkbox } from 'antd-mobile'
+import { WingBlank, List, InputItem as _InputItem, WhiteSpace, Radio, Button, Toast, Checkbox } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { register } from '../../redux/actions'
 import styles from './register.module.less'
+import { create, addErrorExplanation } from "ant-design-mobile-form";
 
 const ListItem = List.Item;
-const AgreeItem = Checkbox.AgreeItem;
+
+
+const InputItem = addErrorExplanation(_InputItem);
 
 class Register extends Component {
     state = {
-        username: '', //用户名
-        password: '', //密码
-        password2: '', //确认密码
         type: 'dashen',      //用户类型名称  dashen/boss
+        registerAgree: false,  // 用户协议授权
     }
     //  注册事件
     register = () => {
-        // console.log(this.state)
-        this.props.register(this.state)
+        const { registerAgree, type } = this.state;
+        this.props.form.validateFields(async (errors, value) => {
+            if(value.password !== value.password2){
+                Toast.show('两次密码输入不一致！')
+            }else if(!registerAgree) { 
+                Toast.show('请勾选用户协议！')
+            }
+            else if (errors === null && registerAgree) {
+                const params = { ...value, type, registerAgree }
+                this.props.register(params)
+            }
+          });
     }
 
     // 跳转登录页面
@@ -41,6 +52,8 @@ class Register extends Component {
     render() {
         const { type } = this.state;
         const { msg, redirectTo } = this.props.user;
+        const { getFieldDecorator } = this.props.form;
+
         const data = [ 
             { value: 'dashen', label: '求职' },
             { value: 'boss', label: '招聘' }
@@ -59,18 +72,21 @@ class Register extends Component {
                         {msg? <div className="error-msg">{msg}</div>:null}
                         <WhiteSpace />
 
-                        <InputItem onChange={val=>{this.handleChange('username',val)}} placeholder="请输入用户名">用户名:</InputItem>
-                        <WhiteSpace />
+                        { getFieldDecorator("username", {
+                            rules: [{ required: true, message: "用户名不可为空"},]
+                        })(<InputItem placeholder="请输入用户名">用户名：</InputItem>)}
 
-                        <InputItem type="password" onChange={val=>{this.handleChange('password',val)}} placeholder="请输入密码">密&nbsp;&nbsp;&nbsp;码:</InputItem>
-                        <WhiteSpace />
-
-                        <InputItem type="password" onChange={val=>{this.handleChange('password2',val)}} placeholder="请再次输入密码">确认密码:</InputItem>
-                        <WhiteSpace />
+                        { getFieldDecorator("password", {
+                            rules: [{ required: true, message: "密码不可为空"},]
+                        })(<InputItem placeholder="请输入密码">密&nbsp;&nbsp;&nbsp;码：</InputItem>)}
+                        { getFieldDecorator("password2", {
+                            rules: [{ required: true, message: "确认密码不可为空"},]
+                        })(<InputItem placeholder="请输入确认密码">确认密码：</InputItem>)}
 
                         <ListItem>
                             <span>注册意向:</span>
                             {
+                                
                                 data.map(i => (
                                     <Radio className="register-radio" key={i.value} checked={type === i.value} onChange={() => this.handleChange('type', i.value)}>
                                     {i.label}
@@ -83,11 +99,11 @@ class Register extends Component {
                         <Button type="primary" size="small" onClick={this.register}>注册</Button>
                         <WhiteSpace size="lg"/>
                         <div className={styles.footer_link}>
-                            <AgreeItem onChange={e => console.log('checkbox', e)} className="register_agree">
-                                我已同意
-                                <span className={styles.login_btn} onClick={this.showAgreement}>用户协议及隐私政策 </span>
-                                <span className={styles.login_btn} onClick={this.toLogin}>直接登录</span>
-                            </AgreeItem>
+                            {/* <input type="checkbox" className={styles.agreement_btn} onChange={val=>this.handleChange('registerAgree', val)} /> */}
+                            <Checkbox onChange={val=>this.handleChange('registerAgree', val.target.checked)} />
+                            我已同意
+                            <span className={styles.login_btn} onClick={this.showAgreement}>用户协议及隐私政策&nbsp;&nbsp;</span>
+                            <span className={styles.login_btn} onClick={this.toLogin}>直接登录</span>
                         </div>
                         <WhiteSpace size="xl"/>       
                     </List>
@@ -97,7 +113,8 @@ class Register extends Component {
     }
 }
 
+const RegisterWrapper = create()(Register);
 export default connect(
     state=>({user: state.user}),
     {register}
-)(Register)
+)(RegisterWrapper)
