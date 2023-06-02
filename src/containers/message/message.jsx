e47d00c0995f9e59bba042662b1846ccdd6dff31
Snail-Lu <snaillu@yeah.net>
2023-06-02 16:26:23 +0800

    消息列表页优化

--- src/containers/message/message.jsx
+++ src/containers/message/message.jsx
@@ -1,90 +1,48 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { List,Badge, NavBar, Image } from 'antd-mobile';
+import { List, Badge, NavBar, Image } from 'antd-mobile';
 import { useNavigate } from 'react-router-dom';
-/**
- * 
- * 对chatMsgs按chat_id进行分组，并得到每个组的lastMsg组成的数组
- * 1.找出每个聊天的lastMsg,并用一个对象容器来保存{chat_id,lastMsg}
- * 2.得到所有lastMsg的数组
- * 3.对数组进行排序（按create_time降序）
- */
-function getLastMsgs(chatMsgs, userid){
-    //1.
-    const lastMsgObjs = {};
-    chatMsgs.forEach(msg=>{
-        //统计未读消息个数
-        if(msg.to===userid && !msg.read){
-            msg.unReadCount = 1;
-        }else{
-            msg.unReadCount = 0;
-        }
+import { getLastMsgs } from '../../utils/index';
 
-
-        const chatId = msg.chat_id;
-        let lastMsg = lastMsgObjs[chatId];
-        if(!lastMsg){
-            lastMsgObjs[chatId] = msg;
-        }else{
-            const unReadCount = lastMsg.unReadCount + msg.unReadCount;
-            if(msg.create_time>lastMsg.create_time){
-                lastMsgObjs[chatId] = msg;
-            }
-            lastMsgObjs[chatId].unReadCount = unReadCount;
-        }
-    })
-
-    //2.
-    const lastMsgs = Object.values(lastMsgObjs);
-
-    //3.
-    lastMsgs.sort(function(m1,m2){
-        return m2.create_time-m1.create_time;
-    })
-
-    return lastMsgs;
-}
-function Message() {
+export default function Message() {
     const user = useSelector(state => state.user.userInfo)
     const { users, chatMsgs } = useSelector(state => state.chat);
     const navigate = useNavigate()
 
-        //对chatMsgs按chat_id进行分组
-        const clonedChatMsgs = JSON.parse(JSON.stringify(chatMsgs))
-        const lastMsgs = getLastMsgs(clonedChatMsgs, user._id)
-        
-        return (
-            <div>
-                <NavBar className="stick-top" backArrow={false}>CHAT</NavBar>
-                <List style={{marginTop:50, marginBottom: 60}}>
-                    {
-                        lastMsgs.map(msg=>{
-                            const targetId = msg.to === user._id? msg.from:msg.to;
-                            const targetUser = users[targetId];
-                            return (
-                                <List.Item
-                                    extra={<Badge text={msg.unReadCount} />}
-                                    prefix={
-                                        <Image
-                                            src={targetUser.avatar ? require(`../../assets/images/${targetUser.avatar}.png`) : require(`../../assets/images/头像1.png`)}
-                                            style={{ borderRadius: 20 }}
-                                            fit='cover'
-                                            width={40}
-                                            height={40}
-                                        />
-                                    }
-                                    key={msg._id}
-                                    onClick={() => navigate(`/chat/${targetId}`)}
-                                    description={msg.content}
-                                >
-                                    {targetUser.username}
-                                </List.Item>
-                            )
-                        })
-                    }
-                </List>
-            </div>
-        );
+    //对chatMsgs按chat_id进行分组，属于同一个会话的集合到一起
+    const clonedChatMsgs = JSON.parse(JSON.stringify(chatMsgs))
+    const lastMsgs = getLastMsgs(clonedChatMsgs, user._id)
+    
+    return (
+        <div>
+            <NavBar className="stick-top" backArrow={false}>消息</NavBar>
+            <List>
+                {
+                    lastMsgs.map(msg=>{
+                        const targetId = msg.to === user._id? msg.from:msg.to;
+                        const targetUser = users[targetId];
+                        return (
+                            <List.Item
+                                extra={<Badge text={msg.unReadCount} />}
+                                prefix={
+                                    <Image
+                                        src={targetUser.avatar ? require(`../../assets/images/${targetUser.avatar}.png`) : require(`../../assets/images/头像1.png`)}
+                                        style={{ borderRadius: 20 }}
+                                        fit='cover'
+                                        width={40}
+                                        height={40}
+                                    />
+                                }
+                                key={msg._id}
+                                onClick={() => navigate(`/chat/${targetId}`)}
+                                description={msg.content}
+                            >
+                                {targetUser.username}
+                            </List.Item>
+                        )
+                    })
+                }
+            </List>
+        </div>
+    );
 }
-
-export default Message;
--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,46 @@
 export function getRedirectTo(){
     return '/friends';
 }
+
+/**
+ * 对用户消息列表按chat_id进行分组
+ * 1.找出每个聊天的lastMsg,并用一个对象容器来保存{chat_id,lastMsg}
+ * 2.得到所有lastMsg的数组
+ * 3.对数组进行排序（按create_time降序）
+ * @returns {} 并得到每个组的lastMsg组成的数组
+ */
+export function getLastMsgs(chatMsgs, userid){
+    //1.
+    const lastMsgObjs = {};
+    chatMsgs.forEach(msg=>{
+        //统计未读消息个数
+        if(msg.to===userid && !msg.read){
+            msg.unReadCount = 1;
+        }else{
+            msg.unReadCount = 0;
+        }
+
+
+        const chatId = msg.chat_id;
+        let lastMsg = lastMsgObjs[chatId];
+        if(!lastMsg){
+            lastMsgObjs[chatId] = msg;
+        }else{
+            const unReadCount = lastMsg.unReadCount + msg.unReadCount;
+            if(msg.create_time>lastMsg.create_time){
+                lastMsgObjs[chatId] = msg;
+            }
+            lastMsgObjs[chatId].unReadCount = unReadCount;
+        }
+    })
+
+    //2.
+    const lastMsgs = Object.values(lastMsgObjs);
+
+    //3.
+    lastMsgs.sort(function(m1,m2){
+        return m2.create_time-m1.create_time;
+    })
+
+    return lastMsgs;
+}
